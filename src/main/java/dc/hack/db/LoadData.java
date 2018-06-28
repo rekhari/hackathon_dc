417b6402acf4245fcb66f074a404de30ab0f93be
chandrikag <chandrika.ganesan@gmail.com>
2018-06-28 14:09:10 -0400

    Class to connect to db and insert data

---
+++ src/main/java/dc/hack/db/LoadData.java
@@ -0,0 +1,56 @@
+package dc.hack.db;
+
+
+import java.io.File;
+import java.util.Iterator;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class LoadData {
+
+    public static void main(String[] args) throws Exception {
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("transaction");
+
+        JsonParser parser = new JsonFactory().createParser(new File("transactiondata.json"));
+
+        JsonNode rootNode = new ObjectMapper().readTree(parser);
+        Iterator<JsonNode> iter = rootNode.iterator();
+
+        ObjectNode currentNode;
+
+        while (iter.hasNext()) {
+            currentNode = (ObjectNode) iter.next();
+
+            int transId = currentNode.path("transaction_id").asInt();
+
+            try {
+                table.putItem(new Item().withPrimaryKey("transaction_id", transId ));
+                System.out.println("PutItem succeeded: " + transId);
+
+            }
+            catch (Exception e) {
+                System.err.println("Unable to add item: " + transId );
+                System.err.println(e.getMessage());
+                break;
+            }
+        }
+        parser.close();
+    }
+}
--- src/main/java/dc/hack/db/dbConnection.java
+++ src/main/java/dc/hack/db/dbConnection.java
@@ -1,6 +1,26 @@
 package dc.hack.db;
 
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class dbConnection {
 
@@ -31,4 +51,62 @@ public class dbConnection {
 
         return dbConn;
     }
+
+    public static DynamoDB getDynamoDbConnection(){
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+
+        /*
+        BasicAWSCredentials myCredentials = new BasicAWSCredentials("id", "key");
+        AmazonDynamoDB dynaClient = AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(myCredentials))
+                .withRegion("us-west-2").build();
+           */
+
+
+        return dynamoDB;
+
+    }
+
+    public void retrieveData(DynamoDB dynamoDB)throws Exception{
+
+        Table table = dynamoDB.getTable("Movies");
+
+        HashMap<String, String> nameMap = new HashMap<String, String>();
+        nameMap.put("#yr", "year");
+
+        HashMap<String, Object> valueMap = new HashMap<String, Object>();
+        valueMap.put(":yyyy", 1985);
+
+        QuerySpec querySpec = new QuerySpec().withKeyConditionExpression("#yr = :yyyy").withNameMap(nameMap)
+                .withValueMap(valueMap);
+
+        ItemCollection<QueryOutcome> items = null;
+        Iterator<Item> iterator = null;
+        Item item = null;
+
+        try {
+            System.out.println("Movies from 1985");
+            items = table.query(querySpec);
+
+            iterator = items.iterator();
+            while (iterator.hasNext()) {
+                item = iterator.next();
+                System.out.println(item.getNumber("year") + ": " + item.getString("title"));
+            }
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to query movies from 1985");
+            System.err.println(e.getMessage());
+        }
+
+
+    }
+
 }
